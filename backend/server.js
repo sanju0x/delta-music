4a10eddb599a320e3d4b644ea02ac6643203d876
sanju0x <nfinitewater@gmail.com>
2026-02-27 10:41:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://delta-music-4ft5.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
